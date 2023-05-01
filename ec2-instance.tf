f8ba23f84c8c06bf78763e80c3691b0bc13835d6
Asli2022 <ashleyaden4u@hotmail.com>
2023-05-01 12:34:25 +0100

    ec2 instance

---
+++ ec2-instance.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+
+    }
+  }
+}
+
+provider "aws" {
+  profile = "AsliPro"
+  region  = "us-east-1"
+
+}
+
+
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-02396cdd13e9a1257"
+  instance_type = "t2.micro"
+}
